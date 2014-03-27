ce3e69de680727b5f06e74b21940bdede0d60ed3
Adrianka <asanchez@draconiantech.com>
2014-03-26 23:14:06 -0600

    Site

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,9 +1,11 @@
 import izmachiolli.App
+import izmachiolli.Site
 
 class BootStrap {
 
     def init = { servletContext ->
         new App(appName:"Ap Prueba").save(failOnError: true)
+        new Site().save()
     }
     def destroy = {
     }
